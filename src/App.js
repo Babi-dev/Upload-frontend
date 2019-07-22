32c710088cb5f9c66ba2d350a0e769ca6dbfe237
Babi-dev <53186630+Babi-dev@users.noreply.github.com>
2019-07-22 16:00:24 -0300

    mexendo no style
    Final da parte global do style da aplicação.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
 import GlobalStyle from "./styles/global";
+import { Container, Content } from "./styles";
 
 class App extends Component {
   render() {
-    return <GlobalStyle />;
+    return (
+      <Container>
+        <Content>Teste</Content>
+        <GlobalStyle />
+      </Container>
+    );
   }
 }
 
